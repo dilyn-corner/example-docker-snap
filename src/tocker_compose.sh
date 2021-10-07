5113ba80a6af1176430bc77017d0b581ee1ad433
Dilyn Corner <dilyn.corner@canonical.com>
2021-10-07 15:27:32 -0400

    A little more cleanup

--- src/tocker_compose.sh
+++ src/tocker_compose.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#export PATH=$SNAP/docker-bin/usr/bin:$SNAP/docker-bin/bin:$PATH
 export PATH=$SNAP/docker-bin/bin:$PATH
 export PYTHONPATH=$SNAP/docker-bin/lib/python3.6/site-packages:$PYTHONPATH
 docker-compose -f $SNAP/usr/share/composers/hello-nginx.yml up
